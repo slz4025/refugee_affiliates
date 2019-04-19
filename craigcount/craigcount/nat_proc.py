9dc740984b36f48ce4dc052280d6d1e18cbd8152
Sydney Zheng <slzheng@andrew.cmu.edu>
2019-04-19 19:51:27 -0400

    get rid of reference to nationalfile

--- craigcount/craigcount/nat_proc.py
+++ craigcount/craigcount/nat_proc.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-nc = pd.read_csv('nationalfile.txt', sep='|')
-nc = nc[nc['FEATURE_CLASS'].str.match('Civil')]
-nc.to_csv("output_civil.csv")
+#nc = pd.read_csv('nationalfile.txt', sep='|')
+#nc = nc[nc['FEATURE_CLASS'].str.match('Civil')]
+#nc.to_csv("output_civil.csv")
 
 nc = pd.read_csv("output_civil.csv")
 nc = nc[nc["FEATURE_NAME"].str.contains("City of ")]
